45c057c613ed73ea9f340cd3fc8c325881b1707e
Devroy Blake <necrodevz@gmail.com>
2018-11-30 06:30:02 -0500

    Sample db running

--- books.php
+++ books.php
@@ -1,7 +1,7 @@
 <?php 
 
-include 'auth.php';
-include 'db.php';
+include 'backend/auth.php';
+include 'backend/db.php';
 
 $books = array();
 $book = [
@@ -16,7 +16,7 @@ $book = [
 
 function getBooks(){
     $result = getTableData('books');
-    $row = mysqli_fetch_array($result);
+    $rows = mysqli_fetch_all($result);
 }
 
 
@@ -38,6 +38,51 @@ function getBooks(){
     </style>
 </head>
 <body>
+
+    <div class="container  fluid">
+        <div class="row" >
+            <div class="col-md-3 col-md-offset-1">
+                <a class="btn btn-primary" href="/book/backend/logout.php" role="button">Logout</a>
+            </div>
+            <div class="col-md-7 col-md-offset-1">
+                <div class="row">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                            <a class="nav-link"  href="/book/dashboard.php">Dash</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle active" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Books</a>
+                            <div class="dropdown-menu">
+                            <a class="dropdown-item" href="/book/books.php">View Books</a>
+                            <a class="dropdown-item" href="/book/backend/add-book.php">Add a Book</a>
+                            <!--
+                            <a class="dropdown-item" href="#"></a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Separated link</a>
+        -->    
+                        </div>
+                        </li>
+                        <!--
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+        -->
+                    </ul>
+                </div>
+                <div class="row">
+                    
+                </div>    
+                <div class="row">
+                    <a class="btn btn-outline-primary dash-btn" href="/book/backend/issue-book.php" role="button">Issue Book</a>
+                    <a class="btn btn-outline-primary dash-btn" href="/book/backend/return-book.php" role="button">Accept Return</a>
+                    <a class="btn btn-outline-primary dash-btn" href="#" role="button">File Complaint</a>
+                </div>    
+            </div>
+        </div>
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
