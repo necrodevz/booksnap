8ddb4d8b834bfa6f2fe1c77e07acc946bf7222da
Devroy Blake <necrodevz@gmail.com>
2018-11-30 06:18:06 -0500

    Dashboard module complete

--- dashboard.php
+++ dashboard.php
@@ -13,6 +13,13 @@
         body{
             padding: 20px;
         }
+        .dash-btn {
+            margin:35px;
+        }
+        .row {
+            margin: 10px;
+            padding:20px;
+        }
     </style>
 </head>
 <body>
@@ -22,36 +29,44 @@
                 <a class="btn btn-primary" href="/book/backend/logout.php" role="button">Logout</a>
             </div>
             <div class="col-md-7 col-md-offset-1">
-                <ul class="nav nav-pills">
-                    <li class="nav-item">
-                        <a class="nav-link"  href="/book/dashboard.php">Dash</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Books</a>
-                        <div class="dropdown-menu">
-                        <a class="dropdown-item" href="/book/books.php">View Books</a>
-                        <a class="dropdown-item" href="/book/backend/add-book.php">Add a Book</a>
+                <div class="row">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                            <a class="nav-link active"  href="/book/dashboard.php">Dash</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Books</a>
+                            <div class="dropdown-menu">
+                            <a class="dropdown-item" href="/book/books.php">View Books</a>
+                            <a class="dropdown-item" href="/book/backend/add-book.php">Add a Book</a>
+                            <!--
+                            <a class="dropdown-item" href="#"></a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Separated link</a>
+        -->    
+                        </div>
+                        </li>
                         <!--
-                        <a class="dropdown-item" href="#"></a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Separated link</a>
-    -->    
-                    </div>
-                    </li>
-                    <!--
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-    -->
-                </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+        -->
+                    </ul>
+                </div>
+                <div class="row">
+                    <a class="btn btn-outline-primary dash-btn" href="/book/backend/add-member.php" role="button">New Member</a>
+                    <a class="btn btn-outline-primary dash-btn" href="/book/backend/disable-member.php" role="button">Cancel Membership</a>
+                </div>    
+                <div class="row">
+                    <a class="btn btn-outline-primary dash-btn" href="/book/backend/issue-book.php" role="button">Issue Book</a>
+                    <a class="btn btn-outline-primary dash-btn" href="/book/backend/return-book.php" role="button">Accept Return</a>
+                    <a class="btn btn-outline-primary dash-btn" href="#" role="button">File Complaint</a>
+                </div>    
             </div>
     </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
+    
+    
